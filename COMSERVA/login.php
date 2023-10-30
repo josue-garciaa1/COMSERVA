795dc8b9344aa1bfbe464806afe069ad8ec964e2
josue-garciaa1 <113656259+josue-garciaa1@users.noreply.github.com>
2023-10-30 12:14:02 -0600

    Add files via upload

---
+++ COMSERVA/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <img src="C:/Users/josue/Desktop/COMSERVA/src/principal.png" alt="Logo" class="logo">
+  <div class="container">
+    <div class="login-form">
+      <h2>Iniciar sesión</h2>
+      <form method="post" action="process-login.php">
+        <input type="text" name="username" placeholder="Usuario" required>
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+    <!-- Other HTML elements... -->
+  </div>
+</body>
+</html>
